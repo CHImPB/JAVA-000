c7076fc8b18a69d76a91c410682eeba8ab3025e9
Zhanghongyu <rainsemail@126.com>
2020-10-28 03:23:10 +0800

    使用HttpClienthttp://localhost:8801

---
+++ Week_02/RequestNIO.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+
+public class RequestNIO {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
+
+        CloseableHttpResponse response;
+        try {
+            response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            System.out.println("响应状态码：" + response.getStatusLine());
+            String result = entity.getContent().toString();
+            System.out.println("返回内容：" + result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
