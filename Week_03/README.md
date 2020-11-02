c1e51a3ed2a1d56420822f14e928f5ca07473a4c
Zhanghongyu <rainsemail@126.com>
2020-11-03 00:05:20 +0800

    周四作业：整合上次作业的 httpclient/okclient;

--- Week_03/README.md
+++ Week_03/README.md
@@ -1,1 +1,36 @@
-学习笔记
+- 课后作业
+
+> 1、按今天的课程要求，实现一个网关，基础代码可以fork:https://github.com/kimmking/JavaCourseCodes/tree/main/02nio/nio02 文件夹下，实现以后代码提交到github
+>
+> 1) 周四作业：整合你上次作业的 httpclient/okclient;
+>
+> 2) 周四作业(可选)：使用netty 实现后端 http访问（代替上一步）
+>
+> 3) 周六作业：实现过滤器 
+
+```java
+@Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+//            String uri = fullRequest.uri();
+//            //logger.info("接收到的请求url为{}", uri);
+//            if (uri.contains("/test")) {
+//                handlerTest(fullRequest, ctx);
+//            }
+            //课程中提到：在这里加filter
+            //1、在这里将request的header中增加 nio:你的名字
+            //	使用场景：在一定条件下可以控制request header中添加头的内容，在handler中实现路由，将请求			//发送给不同的服务
+            //2、在hanhler中创建新的request将传入的request头都取出来放到新的request里
+            
+            handler.handle(fullRequest, ctx);
+    
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+```
+> 4) 周六作业(可选): 实现路由 
--- Week_03/nio02/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundHandler.java
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundHandler.java
@@ -1,10 +1,12 @@
 package io.github.kimmking.gateway.inbound;
 
+import io.github.kimmking.gateway.outbound.httpclient.HttpOutboundHandlerMy;
 import io.github.kimmking.gateway.outbound.httpclient4.HttpOutboundHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.util.ReferenceCountUtil;
+import org.apache.http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,11 +14,11 @@ public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
 
     private static Logger logger = LoggerFactory.getLogger(HttpInboundHandler.class);
     private final String proxyServer;
-    private HttpOutboundHandler handler;
+    private HttpOutboundHandlerMy handler;
     
     public HttpInboundHandler(String proxyServer) {
         this.proxyServer = proxyServer;
-        handler = new HttpOutboundHandler(this.proxyServer);
+        handler = new HttpOutboundHandlerMy(this.proxyServer);
     }
     
     @Override
@@ -34,7 +36,6 @@ public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
 //            if (uri.contains("/test")) {
 //                handlerTest(fullRequest, ctx);
 //            }
-    
             handler.handle(fullRequest, ctx);
     
         } catch(Exception e) {
